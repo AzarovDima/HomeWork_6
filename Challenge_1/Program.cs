f1d53d452971502c7e49e9039c3314be4ac6d97e
AzarovDima <ada89885308140@gmail.com>
2023-01-24 21:58:54 +0300

    Поправил чутка

--- Challenge_1/Program.cs
+++ Challenge_1/Program.cs
@@ -1,10 +1,22 @@
 ﻿/* Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь. */
 
-   
 
-Console.Write("Введите элементы(через пробел): ");
 
-double[] array = Array.ConvertAll((Console.ReadLine() ?? "").Split(), double.Parse);
+double[] array;
+while (true)
+{
+    try
+    {
+        Console.Write("Введите элементы(через пробел): ");
+        array = Array.ConvertAll((Console.ReadLine() ?? "").Split(), double.Parse);
+        break;
+    }
+    catch (Exception exc)
+    {
+        Console.WriteLine($"Ошибка ввода! {exc.Message}");
+    }
+}
+
 double count = 0;
 
 for (int i = 0; i < array.Length; i++)
