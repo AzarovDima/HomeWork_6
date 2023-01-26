f3ab6f36b72b2cd927eae2f63ea53e4c06c1dc4f
AzarovDima <ada89885308140@gmail.com>
2023-01-26 19:50:35 +0300

    Решил вспонмнить

---
+++ 3/Program.cs
@@ -0,0 +1,26 @@
+﻿
+int[] array;
+while (true)
+{
+    try
+    {
+        Console.Write("Введите шесть целых чисел (через пробел): ");
+        array = Array.ConvertAll((Console.ReadLine() ?? "").Split(), int.Parse);
+        break;
+    }
+    catch (Exception exc)
+    {
+        Console.WriteLine($"Ошибка ввода! {exc.Message}");
+    }
+}
+
+int count = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if ((array[i] < 0) & (array[i] % 5 == 0) & (i < 6))
+    {
+        count++;
+    }
+}
+Console.WriteLine($"Количесство отрицательных чисел кратных 5 равно: {count}");
---
+++ 4/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Даны 600 чисел");
+int[] array = NewArray(600, 0, 1000);
+Console.Write($"{String.Join(" ", array)}");
+int sum = Summa(array);
+Console.WriteLine();
+Console.WriteLine($"Количество чисел больше 100 и кратных 3 равно: {sum}");
+int[] NewArray(int size, int minValue, int maxValue)
+{
+    int[] array = new int[size];
+    var rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(minValue, maxValue);
+    }
+    return array;
+}
+
+int Summa(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if ((array[i] > 100) & (array[i] % 3 == 0))
+            sum = sum + 1;
+
+    }
+    return sum;
+}
